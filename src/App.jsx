46259ae2212b1cf305ff74fa15db36afdefa29bb
grandmostach0 <kreedlegend0@gmail.com>
2024-01-25 13:35:53 -0600

    modificando App para agregar el componente nuevo Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from "react";
-import './app.css';
+import Menu from './Menu';
 
 function App(){
     return(
-        <div className="menu">
-            <img id="logoPersonal" src="src\assets\icons\i.svg" alt="imagen" />
-            <nav className="menu-opciones">
-                <ul>
-                    <li><a href="#">about</a></li>
-                    <li><a href="#">proyects</a></li>
-                    <li><a href="#">download cv</a></li>
-                </ul>
-            </nav>
-        </div>
+        <Menu />
     );
 }
 

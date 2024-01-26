92536393d415e4e1103b92354c5e17139e8e3cdd
grandmostach0 <kreedlegend0@gmail.com>
2024-01-26 12:18:28 -0600

    modificando el header dentro del App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react';
 import Menu from './Menu';
 import Cuerpo from './Cuerpo';
+import './menu.css';
 
 function App(){
     return(
         <div>
-            <Menu />
+            <header className="menu">
+                <img id="logoPersonal" src="src\assets\icons\i.svg" alt="imagen" />
+                <Menu/>
+            </header>
             <Cuerpo />
         </div>
     );
--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import './menu.css'
+import './menu.css';
 
 function Menu(){
     return(
-        <div className='menu'>
-            <nav className="menu-opciones">
-                <ul>
-                    <li><a href="#">about</a></li>
-                    <li><a href="#">proyects</a></li>
-                    <li><a href="#">download cv</a></li>
-                </ul>
-            </nav>
-        </div>
+        <nav className="menu-opciones">
+            <ul>
+                <li><a href="#">about</a></li>
+                <li><a href="#">proyects</a></li>
+                <li><a href="#">download cv</a></li>
+            </ul>
+        </nav>
     );
 }
 

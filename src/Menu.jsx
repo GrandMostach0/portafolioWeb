ebc2bb9db68c4914074b061e039b9916e258a147
grandmostach0 <kreedlegend0@gmail.com>
2024-01-26 12:08:02 -0600

    eliminando el logo personal del Menu.jsx para dejar solo las opciones del Menu

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -4,7 +4,6 @@ import './menu.css'
 function Menu(){
     return(
         <div className='menu'>
-            <img id="logoPersonal" src="src\assets\icons\i.svg" alt="imagen" />
             <nav className="menu-opciones">
                 <ul>
                     <li><a href="#">about</a></li>

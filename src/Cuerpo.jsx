33fa82b8064d77f0f5d6fe65fe5cdb48e7e524b7
grandmostach0 <kreedlegend0@gmail.com>
2024-01-26 13:09:40 -0600

    agregando todo lo necesario para la presentacion

--- src/Cuerpo.jsx
+++ src/Cuerpo.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import './cuerpo.css';
+import './main.css';
 
 function Cuerpo(){
     return(
         <div className='presentacion'>
             <div className='presentacion-text'>
-                <h1>Front-end Developer</h1>
-                <p>Focused on creating website and web applications. I like what i do ❤️</p>
+                <h1 className='titulos'>Front-end Developer</h1>
+                <p className='parrafos'>Focused on creating website and web applications. I like what i do ❤️</p>
             </div>
             <img src="src/assets/images/foto.png" alt="photoLead" />
         </div>

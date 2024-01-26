1f93b2e8b4a5c6ccd0a2142b16c8dfc2ea0aa251
grandmostach0 <kreedlegend0@gmail.com>
2024-01-26 13:17:25 -0600

    proceso de la presentacion

--- src/Cuerpo.jsx
+++ src/Cuerpo.jsx
@@ -8,8 +8,14 @@ function Cuerpo(){
             <div className='presentacion-text'>
                 <h1 className='titulos'>Front-end Developer</h1>
                 <p className='parrafos'>Focused on creating website and web applications. I like what i do ❤️</p>
+                <section className='seccion-sociales-botones'>
+                    <a href="#"><img src="" alt="git-hub"/></a>
+                    <a href="#"><img src="" alt="linkedln"/></a>
+                    <a href="#"><img src="" alt="behance"/></a>
+                    <a href="#"><img src="" alt="twitter"/></a>
+                </section>
             </div>
-            <img src="src/assets/images/foto.png" alt="photoLead" />
+            <img id='foto-personal' src="src/assets/images/foto.png" alt="photoLead" />
         </div>
     );
 }

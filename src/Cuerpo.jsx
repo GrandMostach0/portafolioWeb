2c9b75a388aaa680070374329679969a01797a67
grandmostach0 <kreedlegend0@gmail.com>
2024-01-26 13:37:19 -0600

    agregando los enlaces a los ancors

--- src/Cuerpo.jsx
+++ src/Cuerpo.jsx
@@ -9,9 +9,9 @@ function Cuerpo(){
                 <h1 className='titulos'>Front-end Developer</h1>
                 <p className='parrafos'>Focused on creating website and web applications. I like what i do ❤️</p>
                 <section className='seccion-sociales-botones'>
-                    <a href="#"><img src="src/assets/icons/github-mark/github-mark-white.svg" alt="git-hub"/></a>
-                    <a href="#"><img src="src/assets/icons/icon-linkedlin.svg" alt="linkedln"/></a>
-                    <a href="#"><img src="src/assets/icons/icon-behance.svg" alt="behance"/></a>
+                    <a href="https://github.com/GrandMostach0" target='_blank'><img src="src/assets/icons/github-mark/github-mark-white.svg" alt="git-hub"/></a>
+                    <a href="https://www.linkedin.com/in/victorchanvarguez/" target='_blank'><img src="src/assets/icons/icon-linkedlin.svg" alt="linkedln"/></a>
+                    <a href="https://www.behance.net/victorchanva" target='_blank'><img src="src/assets/icons/icon-behance.svg" alt="behance"/></a>
                     <a href="#"><img src="src/assets/icons/icon-twitter.svg" alt="twitter"/></a>
                 </section>
             </div>

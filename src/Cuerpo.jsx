3d52a9fc60ea4d229714b959fae914a137ed0477
grandmostach0 <kreedlegend0@gmail.com>
2024-01-29 16:52:15 -0600

    cambiando el div --> section en el componente Cuerpo

--- src/Cuerpo.jsx
+++ src/Cuerpo.jsx
@@ -4,7 +4,7 @@ import './main.css';
 
 function Cuerpo(){
     return(
-        <div className='presentacion'>
+        <section className='presentacion'>
             <div className='presentacion-text'>
                 <h1 className='titulos'>Front-end Developer</h1>
                 <p className='parrafos'>Focused on creating website and web applications. I like what i do ❤️</p>
@@ -20,7 +20,7 @@ function Cuerpo(){
                 </section>
             </div>
             <img id='foto-personal' src="src/assets/images/foto.png" alt="photoLead" />
-        </div>
+        </section>
     );
 }
 

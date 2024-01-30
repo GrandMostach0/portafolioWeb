90ccbf8fdc3c757da1b234d565d7529464da7957
grandmostach0 <kreedlegend0@gmail.com>
2024-01-29 18:23:50 -0600

    agregando información

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,10 @@ import './cuerpo.css';
 function Cuerpo2(){
     return(
         <section className='separador'>
-            
+            <div className="container-about">
+                <div className="about-title"></div>
+                <p>Me gusta diseñar y desarollar páginas web o aplicaciones web, actualmente estoy trabajando en pequeños proyectos que me han ayudado a mejorar mis habilidades tanto de diseño como desarrollador web.</p>
+            </div>
         </section>
     );
 }

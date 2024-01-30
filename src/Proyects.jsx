bc77b2d75ab9175ff951d81b0006b10303490430
grandmostach0 <kreedlegend0@gmail.com>
2024-01-29 19:24:22 -0600

    estructurando mas informacion

--- src/Proyects.jsx
+++ src/Proyects.jsx
@@ -7,6 +7,9 @@ function Proyects(){
     return(
         <section className='container-proyects'>
             <h1 className='titulos'>Proyectos</h1>
+            <div className="container-proyectos-cards">
+                
+            </div>
         </section>
     )
 }

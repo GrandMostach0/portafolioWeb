35c1775fa0d109597dba103f69450d482ff4f89e
grandmostach0 <kreedlegend0@gmail.com>
2024-01-29 19:17:49 -0600

    creando la hoja de estilos para Proyects

--- src/Proyects.jsx
+++ src/Proyects.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './cuerpo.css';
+import './about.css';
 
 function Proyects(){
     return(
-        <Section>
-            <h1>h</h1>
-        </Section>
+        <section className='container-proyects'>
+            <h1 className='titulos'>Proyectos</h1>
+        </section>
     )
 }
 

284a01654603fe9c8fe8af56042e99fbb0313c21
grandmostach0 <kreedlegend0@gmail.com>
2024-01-30 18:52:40 -0600

    props para el componente proyect para realizar mas fafacil el llanado de nuevo proyectos

--- src/Proyects.jsx
+++ src/Proyects.jsx
@@ -4,17 +4,31 @@ import './cuerpo.css';
 import './about.css';
 import './proyects.css';
 
+const cards = [
+    {
+        id: 1,
+        title: 'fazt Web'
+    },
+    {
+        id: 2,
+        title: 'fazt blog'
+    },
+    {
+        id: 3,
+        title: 'fazt youtube'
+    }
+]
+
 function Proyects(){
     return(
         <section className='container-proyects'>
             <h1 className='titulos'>Proyectos</h1>
             <div className="container-proyects-cards">
-                <Proyect />
-                <Proyect />
-                <Proyect />
-                <Proyect />
-                <Proyect />
-                <Proyect />
+                {
+                    cards.map(proyect => (
+                        <Proyect key={proyect.id} title={proyect.title}/>
+                    ))
+                }
             </div>
         </section>
     )
--- src/components/proyect.jsx
+++ src/components/proyect.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './proyect.css';
 
-export default function proyect() {
+export default function proyect(props) {
   return (
-    <div className='card-proyect'>
+    <div className='card-proyect' key={props.id}>
         <div className='card-proyect-title'>
-            <h1>Titulo Del proyecto</h1>
+            <h1>{props.title}</h1>
         </div>
         <div className='card-proyect-buttons'>
             <button>Boton 1</button>

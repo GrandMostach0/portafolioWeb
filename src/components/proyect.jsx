a317f13af75ef67a67ffb95aa8c6506cab5a892d
grandmostach0 <kreedlegend0@gmail.com>
2024-01-30 18:35:24 -0600

    modificando las cards y eliminacion del parrafo

--- src/components/proyect.jsx
+++ src/components/proyect.jsx
@@ -7,8 +7,6 @@ export default function proyect() {
         <div className='card-proyect-title'>
             <h1>Titulo Del proyecto</h1>
         </div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur neque, nihil beatae quam, incidunt repudiandae aliquid reprehenderit unde, provident odio sunt nam fuga? Optio facere numquam iusto, reiciendis eius incidunt.
-        Consequuntur iusto nostrum fuga. Ipsam dolorem odit ad in error accusantium voluptatum. Nulla unde voluptates veniam neque odio laborum quam dolorem ipsum iure ullam. Est voluptatibus nesciunt ut odit minima!</p>
         <div className='card-proyect-buttons'>
             <button>Boton 1</button>
             <button>Buton 2</button>

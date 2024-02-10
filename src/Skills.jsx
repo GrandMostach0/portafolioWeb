e8ca8832817de527ba4217b81c1a83c0b4900bd5
grandmostach0 <kreedlegend0@gmail.com>
2024-02-09 21:06:52 -0600

    modificando el estilo de los skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,21 +1,56 @@
 import React from 'react'
+import Skill from './components/Skill';
 import './proyects.css';
 import './about.css';
 import './skills.css';
+
+const skillsList = [
+  {
+    id: 1,
+    titleSkill: 'Java'
+  },
+  {
+    id: 2,
+    titleSkill: 'Figma'
+  },
+  {
+    id: 3,
+    titleSkill: 'C++'
+  },
+  {
+    id: 4,
+    titleSkill: 'GitHub + Git'
+  },
+  {
+    id: 5,
+    titleSkill: 'Docker'
+  },
+  {
+    id: 6,
+    titleSkill: 'Linux/Windows'
+  },
+  {
+    id: 7,
+    titleSkill: 'Mysql'
+  },
+  {
+    id: 8,
+    titleSkill: 'React'
+  }
+]
 
 export default function Skills() {
   return (
     <section className='container-proyects'>
         <h1 className='titulos'>Skills</h1>
         <div className='container-skills'>
-            <div className='skill'>JavaScript</div>
-            <div className='skill'>Java</div>
-            <div className='skill'>Figma</div>
-            <div className='skill'>C++</div>
-            <div className='skill'>GitHub</div>
-            <div className='skill'>Docker</div>
-            <div className='skill'>SO Linux/Windows</div>
-            <div className='skill'>Mysql</div>
+          {
+            skillsList.map(skill => (
+              <div className='skill' key={skill.id}>
+                <Skill titleSkill={skill.titleSkill}/>
+              </div>
+            ))
+          }
         </div>
     </section>
   )
---
+++ src/components/Skill.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../skills.css';
+
+function Skill(props) {
+  return (
+    <h4>{props.titleSkill}</h4>
+  )
+}
+
+export default Skill

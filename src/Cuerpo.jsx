797d3a5188bbcf053afe73eaac464998a3789068
grandmostach0 <kreedlegend0@gmail.com>
2024-02-09 20:32:36 -0600

    modificando el marguen de mi prensentacion para que funcione mejor

--- src/Cuerpo.jsx
+++ src/Cuerpo.jsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import FotoPersonal from './assets/images/foto.png';
+import LogoGit from './assets/icons/github-mark/github-mark-white.svg';
+import LogoLinkedlin from './assets/icons/icon-linkedlin.svg';
+import LogoBehance from './assets/icons/icon-behance.svg';
+import LogoTwitter from './assets/icons/icon-twitter.svg';
 import './cuerpo.css';
 import './main.css';
 
@@ -10,17 +15,17 @@ function Cuerpo(){
                 <h1 className='titulos'>Front-end Developer</h1>
                 <p className='parrafos'>Focused on creating website and web applications. I like what i do ❤️</p>
                 <section className='seccion-sociales-botones'>
-                    <a href="https://github.com/GrandMostach0" target='_blank'><img src="src/assets/icons/github-mark/github-mark-white.svg" alt="git-hub"/></a>
-                    <a href="https://www.linkedin.com/in/victorchanvarguez/" target='_blank'><img src="src/assets/icons/icon-linkedlin.svg" alt="linkedln"/></a>
-                    <a href="https://www.behance.net/victorchanva" target='_blank'><img src="src/assets/icons/icon-behance.svg" alt="behance"/></a>
-                    <a href="#"><img src="src/assets/icons/icon-twitter.svg" alt="twitter"/></a>
+                    <a href="https://github.com/GrandMostach0" target='_blank'><img src={LogoGit} alt="git-hub"/></a>
+                    <a href="https://www.linkedin.com/in/victorchanvarguez/" target='_blank'><img src={LogoLinkedlin} alt="linkedln"/></a>
+                    <a href="https://www.behance.net/victorchanva" target='_blank'><img src={LogoBehance} alt="behance"/></a>
+                    <a href="#"><img src={LogoTwitter} alt="twitter"/></a>
                 </section>
                 <section className='seccion-sociales-botones seccion-botones'>
                     <button>Contacto</button>
                     <button>Descargar CV</button>
                 </section>
             </div>
-            <img id='foto-personal' src="src/assets/images/foto.png" alt="photoLead" />
+            <img id='foto-personal' src={FotoPersonal} alt="photoLead" />
         </section>
     );
 }

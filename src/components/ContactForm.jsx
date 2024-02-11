30ef55691a6e4e5d5087bc94f5e718f61de5d6b0
grandmostach0 <kreedlegend0@gmail.com>
2024-02-11 10:48:36 -0600

    agregando detalles a mi formulario de contacto

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import FotoPersonal from '../assets/images/foto.png';
+import LogoGit from '../assets/icons/github-mark/github-mark-white.svg';
+import LogoLinkedlin from '../assets/icons/icon-linkedlin.svg';
+import LogoBehance from '../assets/icons/icon-behance.svg';
+import LogoTwitter from '../assets/icons/icon-twitter.svg';
 import './contactform.css';
 
+//componente para relalizar el cipboard
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+ 
 function ContactForm() {
 
   const {register, handleSubmit} = useForm ();
@@ -12,6 +20,7 @@ function ContactForm() {
 
   return (
     <div className='separador'>
+      
       <div className='container-form-contact'>
 
         <form className='formulario' onSubmit={handleSubmit(onSubmit)}>
@@ -19,28 +28,46 @@ function ContactForm() {
           <div className='separador-form-div-name-email'>
             
             <div className='wrapper'>
-              <div className='input-data'>
-                <input type='text' {...register('name')}/>
-                <label htmlFor='name'>Nombre*</label>
-              </div>
+              <label className='lbl-nombre' htmlFor='name'>Nombre</label>
+              <input autoComplete='off' type='text' {...register('name')}/>
             </div>
 
             <div div className='wrapper'>
-              <input type='email' {...register('email')}/>
-              <label className='lbl-nombre' htmlFor='email'>Email*</label>
+              <label className='lbl-nombre' htmlFor='email'>Email</label>
+              <input autoComplete='off' type='email' {...register('email')}/>
             </div>
 
           </div>
 
-          <div>
-            <textarea name="message" className='textarea-responsive'></textarea>
+          <div className='container-textarea'>
+            <label className='lbl-nombre' htmlFor="message">Message</label>
+            <textarea name="message" className='textarea-responsive' {...register('message')}/>
           </div>
 
-          <button type='submit'>Enviar</button>
+          <button className='sendMessage' type='submit'>Enviar</button>
         </form>
         
         <div className='contact'>
-          <h2>Hola Mundo</h2>
+          <h2 className='contact-title'>Let's Talk</h2>
+          <p>More information contact:</p>
+          <div className='container-info'>
+            <div className='info'>
+              <CopyToClipboard text='mi text a pegar'>
+                <p>Copy me!!</p>
+              </CopyToClipboard>
+            </div>
+            <div className='info'>
+
+            </div>
+            <div className="info cards">
+              <section className='seccion-sociales-botones'>
+                    <a href="https://github.com/GrandMostach0" target='_blank'><img src={LogoGit} alt="git-hub"/></a>
+                    <a href="https://www.linkedin.com/in/victorchanvarguez/" target='_blank'><img src={LogoLinkedlin} alt="linkedln"/></a>
+                    <a href="https://www.behance.net/victorchanva" target='_blank'><img src={LogoBehance} alt="behance"/></a>
+                    <a href="#"><img src={LogoTwitter} alt="twitter"/></a>
+                </section>
+            </div>
+          </div>
         </div>
       </div>
     </div>

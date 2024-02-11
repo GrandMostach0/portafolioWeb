2e9f6f7dba7da22f72d298b0b3bb0576cc20d1fb
grandmostach0 <kreedlegend0@gmail.com>
2024-02-10 22:45:26 -0600

    realizando muchos pero muchos cambios que no se jajaja

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
-import '../about.css';
+import { useForm } from 'react-hook-form';
+import './contactform.css';
 
 function ContactForm() {
+
+  const {register, handleSubmit} = useForm ();
+
+  const onSubmit = (data) =>{
+    console.log(data);
+  }
+
   return (
     <div className='separador'>
-        <form action="" method="post">
-            <input type="text" />
-            <button type='submit'>Enviar</button>           
+      <div className='container-form-contact'>
+
+        <form className='formulario' onSubmit={handleSubmit(onSubmit)}>
+
+          <div className='separador-form-div-name-email'>
+            
+            <div className='wrapper'>
+              <div className='input-data'>
+                <input type='text' {...register('name')}/>
+                <label htmlFor='name'>Nombre*</label>
+              </div>
+            </div>
+
+            <div div className='wrapper'>
+              <input type='email' {...register('email')}/>
+              <label className='lbl-nombre' htmlFor='email'>Email*</label>
+            </div>
+
+          </div>
+
+          <div>
+            <textarea name="message" className='textarea-responsive'></textarea>
+          </div>
+
+          <button type='submit'>Enviar</button>
         </form>
+        
+        <div className='contact'>
+          <h2>Hola Mundo</h2>
+        </div>
+      </div>
     </div>
   )
 }

9206babc6f4be9e7528889d8b8a0502176712d2f
grandmostach0 <kreedlegend0@gmail.com>
2024-02-22 00:03:11 -0600

    50 porciento de vance en la estructura de las cards

--- src/Proyects.jsx
+++ src/Proyects.jsx
@@ -11,7 +11,8 @@ const cards = [
         id: 1,
         category: 'FullStack',
         title: 'fazt Web',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.',
+        classNameP: 'wide'
     },
     {
         id: 2,
@@ -21,27 +22,13 @@ const cards = [
     {
         id: 3,
         title: 'fazt youtube',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.',
     },
     {
         id: 4,
         title: 'fazt Web',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.'
-    },
-    {
-        id: 5,
-        title: 'fazt blog',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.'
-    },
-    {
-        id: 6,
-        title: 'fazt youtube',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.'
-    },
-    {
-        id: 7,
-        title: 'fazt Networks',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.'
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.',
+        classNameP: 'wide'
     }
 ]
 
@@ -57,15 +44,15 @@ function Proyects(){
                         cards.map(proyect => (
 
                             <div 
-                            className='card-proyect' 
-                            key={proyect.id}
-                            data-aos = "fade-right">
+                            className={`card-proyect ${proyect.classNameP || 'default-class'}`}
+                            key={proyect.id}>
                                 <Proyect
                                 category={proyect.category}
                                 title={proyect.title}
-                                description={proyect.description}/>
+                                description={proyect.description}
+                                claseName={proyect.classNameP}/>
                             </div>
-                            
+
                         ))
                     )
                 }
--- src/components/proyect.jsx
+++ src/components/proyect.jsx
@@ -3,16 +3,18 @@ import LogoGitIcon from './icons/GitHubIcon';
 import './proyect.css';
 
 import PlaceDilios from '../assets/images/placeDelirios.png';
+import GitHubIcon from './icons/GitHubIcon';
+import BrowserIcon from './icons/BrowserIcons';
 
 export default function proyect(props) {
   return (
     <>
-      <div className='card-proyect-img'>
+      <div className={`card-proyect-img ${proyect.classNameP || 'default-class'}`}>
         <img src={PlaceDilios} alt="Place and Deliruos" />
       </div>
       <div className='card-proyect-title-description'>
         <h1>{props.title}</h1>
-        <div className='tags'>
+        <div className='category tags'>
           <p>{props.category}</p>
         </div>
         <p>{props.description}</p>
@@ -30,10 +32,13 @@ export default function proyect(props) {
           </div>
           
         </div>
+
         <div className='card-proyect-buttons'>
           <div className='button'>
-            <a className='title-buton' href="">Code</a>
-            <LogoGitIcon />
+            <a className='title-buton' href=""> <GitHubIcon color={"#ececec"} /> Code </a>
+          </div>
+          <div className='button'>
+            <a className='title-buton' href=""> <BrowserIcon color={"#ececec"} /> Preview </a>
           </div>
         </div>
       </div>

62e37b0f4e8cd736d655c46adacb1f82a1a968f7
grandmostach0 <kreedlegend0@gmail.com>
2024-02-27 14:49:44 -0600

    funcionamiento basico del theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu';
 import Cuerpo from './Cuerpo';
 import About from './About';
@@ -7,25 +7,41 @@ import Skills from './Skills';
 import Footer from './components/Footer';
 import Contact from './components/ContactForm';
 import './app.css';
-import 'aos/dist/aos.css';
+// Supports weights 100-900
+import '@fontsource-variable/onest';
+import { Toggle } from './components/Toggle';
+
+import useLocalStorage from 'use-local-storage';
 
 function App(){
-    return(
-        <div className='App appfinal'>
-            <Menu/>
-            <div className="circulo"></div>
-            <div className="circulo c-1"></div>
+    //esta constante obtiene la preferencia del tema del systema, si es dark entonces la página comienza con tema dark y viceversa
+    const preference = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    console.log("Preferencia del sistema:", preference);
 
-            <div className="container-app">
-                <Cuerpo />
-                <Proyects />
-                <About  />
-                <Skills />
-                <Contact />
 
-            </div>
-            <Footer />
+    /*
+    Local storage, para guardar la preferencia ya que cuando se refresca vuelve a dark sin guardar cambios, con el uso de local
+    storage al refrescar se mantiene los cambios*/
+    const [isDark, setIsDark] = useLocalStorage("isDark", preference);
+
+    return (
+      <div className="App appfinal" data-theme={isDark ? "dark" : "light"}>
+        <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
+        <Menu />
+        <div className="circulo"></div>
+        <div className="circulo c-1"></div>
+
+        <div className="container-app">
+          <Cuerpo />
+          <Proyects />
+          <About />
+          <Skills />
+          <Contact />
         </div>
+        <Footer />
+      </div>
     );
 }
 

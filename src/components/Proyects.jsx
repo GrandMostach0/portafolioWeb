5755d0125bdd0573a20f2d914af19360afa7b425
grandmostach0 <kreedlegend0@gmail.com>
2024-03-03 23:34:02 -0600

    creando json para la informacion de los proyectos sin embargo hay un error en la carga de imagenes, pero los datos si cargan, modificacion en la informacion de los proyectos para que mueste informacion segun la informacion del json

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -5,36 +5,7 @@ import '../styles/proyects.css';
 
 const opcionesMenu = ['Web', 'Ciberseguridad', 'Devops'];
 
-const cards = [
-  {
-    id: 1,
-    categoryType: 'Web',
-    title: 'fazt Web Front-End',
-    category: 'FullStack',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.',
-    urlGitHub: 'https://github.com/GrandMostach0',
-    urlPreview: 'https://google.com/',
-  },
-  {
-    id: 2,
-    categoryType: 'Ciberseguridad',
-    title: 'fazt blog Ciberseguridad',
-    category: 'FullStack',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.',
-    urlGitHub: 'https://github.com/GrandMostach0',
-    urlPreview: 'https://google.com/',
-  },
-  {
-    id: 3,
-    categoryType: 'Devops',
-    title: 'fazt youtube Devops',
-    category: 'FullStack',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur minima enim adipisci sequi molestiae, rem maxime laborum reprehenderit suscipit non ipsum, ab ut facilis vel illo dignissimos doloribus doloremque fugiat.',
-    urlGitHub: 'https://github.com/GrandMostach0',
-    urlPreview: 'https://google.com/',
-    classNameP: 'wide',
-  },
-];
+import proyectsData from '../json/proyectsData.json';
 
 function Proyects() {
   const [selectedOption, setSelectedOption] = useState('Web');
@@ -44,46 +15,50 @@ function Proyects() {
   };
 
   const filteredCards = selectedOption
-    ? cards.filter((proyect) => proyect.categoryType.toLowerCase() === selectedOption.toLowerCase())
-    : cards;
+    ? proyectsData.filter((proyect) => proyect.categoryType.toLowerCase() === selectedOption.toLowerCase())
+    : proyectsData;
 
   return (
-    <section className='container-proyects'>
-      <h1 className='titulos titulos-separadores'>Proyectos</h1>
-      <div className='container-menu-proyectos'>
+    <section className="container-proyects">
+      <h1 className="titulos titulos-separadores">Proyectos</h1>
+      <div className="container-menu-proyectos">
         {opcionesMenu.map((opcion, index) => (
           <p
             key={index}
-            className={`opcion-menu ${selectedOption === opcion.toLowerCase() ? 'selected' : ''}`}
+            className={`opcion-menu ${
+              selectedOption === opcion.toLowerCase() ? "selected" : ""
+            }`}
             onClick={() => handleOptionSelected(opcion)}
           >
             {opcion}
           </p>
         ))}
       </div>
-      <div className='container-proyects-cards'>
+      <div className="container-proyects-cards">
         {filteredCards.length === 0 ? (
-          <div className='card-proyect wide'>
+          <div className="card-proyect wide">
             <div className="card-proyect-null">
-                <div className='container-null'>
-                    <ErrorIcon color={"#fff"} classNameP={'error-card'}/>
-                    <p>No hay información 😕</p>
-                </div>
+              <div className="container-null">
+                <ErrorIcon color={"#fff"} classNameP={"error-card"} />
+                <p>No hay información 😕</p>
+              </div>
             </div>
           </div>
         ) : (
           filteredCards.map((proyect, index) => (
             <div
-              className={`card-proyect ${index % 3 === 2 ? 'wide' : 'default'}`}
+              className={`card-proyect ${index % 3 === 2 ? "wide" : "default"}`}
               key={proyect.id}
             >
               <Proyect
                 category={proyect.category}
                 title={proyect.title}
+                imagePath={proyect.imagePath}
                 description={proyect.description}
                 urlGitHub={proyect.urlGitHub}
                 urlPreview={proyect.urlPreview}
                 claseName={proyect.classNameP}
+                tags={proyect.tags}
               />
             </div>
           ))
